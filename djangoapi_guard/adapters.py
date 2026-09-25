0029d0f70dca437498b6f3f0a9758bbc347800d5
Renn F <rennf93@users.noreply.github.com>
2026-09-25 21:36:24 +0200

    style: ruff format adapters

--- djangoapi_guard/adapters.py
+++ djangoapi_guard/adapters.py
@@ -124,7 +124,9 @@ class DjangoGuardResponse:
 class DjangoResponseFactory:
     def create_response(self, content: str, status_code: int) -> DjangoGuardResponse:
         return DjangoGuardResponse(
-            HttpResponse(content, status=status_code, content_type="text/plain; charset=utf-8")
+            HttpResponse(
+                content, status=status_code, content_type="text/plain; charset=utf-8"
+            )
         )
 
     def create_redirect_response(
